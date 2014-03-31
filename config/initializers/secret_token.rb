360cb28749ba9f3a9134edf88a55b80476866bb1
mariam <mariamr@gmail.com>
2014-03-30 23:10:51 -0700

    adding all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhiteBoard::Application.config.secret_token = '33e1b108041254a9c6a845437fcf584874d56d9b57416aacbd9068a810b5a159a0a12e9a07c825dc8e38ed04983b653fa6762bc327106ffde84bda575d28323e'
